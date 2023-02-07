3c9c52da8298725c16334a3ad145329d5c2413be
Florian Micliuc <flmi@itu.dk>
2023-02-07 15:00:48 +0000

    Modify minitwit.py

--- minitwit.py
+++ minitwit.py
@@ -47,7 +47,7 @@ def connect_db():
 def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
+        with app.open_resource('schema.sql','r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
@@ -103,7 +103,7 @@ def timeline():
     redirect to the public timeline.  This timeline shows the user's
     messages as well as all the messages of followed users.
     """
-    print("We got a visitor from: " + str(request.remote_addr))
+    print(("We got a visitor from: " + str(request.remote_addr)))
     if not g.user:
         return redirect(url_for('public_timeline'))
     offset = request.args.get('offset', type=int)
