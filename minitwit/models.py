9b26c3a35b9e58e468762e561f8cf3d61e9d09fc
Thomas <thomas.schauser@gmail.com>
2023-02-28 15:06:47 +0100

    Update local database to postgres

--- minitwit/models.py
+++ minitwit/models.py
@@ -7,8 +7,6 @@ class Follower(models.Model):
     who_id = models.ForeignKey(User, related_name="who", on_delete = models.CASCADE)
     whom_id = models.ForeignKey(User, related_name="whom", on_delete = models.CASCADE)
 
-
-
 class Message(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
