5443d21f30708eaed24f901c9cf13218c2af83aa
Thomas <thomas.schauser@gmail.com>
2023-02-28 18:39:26 +0100

    Switched docker compose

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,5 +2,5 @@ source ~/.bash_profile
 
 cd /vagrant/tools
 
-docker-compose -f docker-compose.yml pull
-docker-compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml pull
+docker compose -f docker-compose.yml up -d
